f982055d4d43341ecab8d9b9e8cbd874dd0db437
Praveenkumar579 <praveenkumarc579@gmail.com>
2021-08-11 01:04:47 +0530

    Simple Aura 10-08

--- force-app/main/default/aura/SimpleAuraComponent/SimpleAuraComponentController.js
+++ force-app/main/default/aura/SimpleAuraComponent/SimpleAuraComponentController.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doInit : function(component, event, helper) {
+        console.log("Came from INit");
+    },
+    SayHello : function (cmp, event, helper) {
+        console.log('Came into Sayhello');
+        const name=cmp.find("nameInput").get("v.value");
+        cmp.set("v.peson", name);
     }
 })
